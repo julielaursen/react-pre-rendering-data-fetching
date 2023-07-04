a65e0e588ea222fdd125d7c0acb3d533b0ea556d
julielaursen <julielaursen1@gmail.com>
2023-07-04 16:24:07 -0500

    Make pid file dynamic

--- pages/[pid].js
+++ pages/[pid].js
@@ -2,8 +2,12 @@ import { Fragment } from 'react'
 import fs from 'fs/promises'
 import path from 'path'
 
-function ProductDetailPage() {
+function ProductDetailPage(props) {
   const { loadedProduct } = props
+
+  if (!loadedProduct) {
+    return <p>Loading...</p>
+  }
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -12,19 +16,37 @@ function ProductDetailPage() {
   )
 }
 
-export async function getStaticProps(context) {
-  const { params } = context
-  const productId = params.pid
-
+async function getData() {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
   const jsonData = await fs.readFile(filePath)
   const data = JSON.parse(jsonData)
+
+  return data
+}
+
+export async function getStaticProps(context) {
+  const { params } = context
+  const productId = params.pid
+  const data = await getData()
 
   const product = data.products.find((product) => product.id === productId)
   return {
     props: {
       loadedProduct: product,
     },
+  }
+}
+
+export async function getStaticPaths() {
+  const data = await getData()
+
+  const ids = data.products.map((product) => product.id)
+
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }))
+
+  return {
+    paths: pathsWithParams,
+    fallback: false,
   }
 }
 
